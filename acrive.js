0dac86611db17b569f2be3f67260a70e86ae15e9
Technjehad <38501938+Technjehad@users.noreply.github.com>
2019-12-01 20:16:41 +0200

    Create acrive.js

---
+++ acrive.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+var btnactivate = "HERE";
+var urlactivate = "YOURURL";
+var messageerror = "Message of Activation";
+
+    var a = $(".quickedit").attr("href"),
+        a = a.substring(35, 54),
+        b = a.substring(0, 1),
+        c = a.substring(1, 2),
+        d = a.substring(2, 3),
+        e = a.substring(3, 4),
+        f = a.substring(4, 5),
+        g = a.substring(5, 6),
+        h = a.substring(6, 7),
+        i = a.substring(7, 8),
+        j = a.substring(8, 9),
+        k = a.substring(9, 10),
+        l = a.substring(10, 11),
+        m = a.substring(11, 12),
+        n = a.substring(12, 13),
+        o = a.substring(13, 16),
+        p = a.substring(16, 19),
+        q = "8" + k + "71" + c + "926" + l + g + "09" + i + "43" + n + "54" + h + "2" + f + "3536" + j + "871" + b + "329" + d + "246" + m + "04253" + b + "53044" + e + "2235" + o + "21252" + i + "005" + h + "25" + b + "310" + d + "02" + g + "10" + p + "70" + l + "72575" + o + "8102" + n + "5342" + e + "85" + j + "7231" + m + e + d + l + p + o + n + m + j + d + "1525";
+
+ $("#activeblog").text() != q && $("body").html('<span><div id="no-active"><span>'+ messageerror +'<a href='+ urlactivate +'> '+btnactivate+'</a></div>');
+
+
+});
